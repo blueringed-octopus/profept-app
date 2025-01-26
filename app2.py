38d22839c82331b021978b2dcc9bbae3593c426a
blueringed-octopus <marcos.pelaes@fatec.sp.gov.br>
2025-01-26 15:46:21 -0300

    "Update 26.01.2025"

--- app2.py
+++ app2.py
@@ -39,6 +39,39 @@ df_questions_list = df_questions_list["Questões"].apply(pd.Series).T.rename(col
 
 questions = df_questions_list["Questões"].unique()
 
+# Displaying the questions
+st.write("## Questões")
+st.write(df_questions_list)
+
+# Displaying general answers frequency to the questions
+
+st.write("## Frequência geral das respostas por pergunta")
+df_questions = df_filtered.iloc[:, 1:]
+
+df_questions_rnm = df_filtered.iloc[:, 1:]
+
+df_questions_rnm.columns = range(1, len(df_questions.columns) + 1)
+
+freq_questions = df_questions.apply(pd.Series.value_counts).fillna(0).T.rename(columns={0: "Questões"})
+
+st.write(freq_questions)
+
+freq_questions_rnm = df_questions_rnm.apply(pd.Series.value_counts).fillna(0).T.rename(columns={0: "Questões"})
+
+# Filtrando as colunas, transformando em linhas e renomeando a nova coluna para "Respostas"
+freq_questions_melted = freq_questions_rnm.reset_index().melt(id_vars=["index"], var_name="Respostas", value_name="Frequência")
+freq_questions_melted = freq_questions_melted.rename(columns={"index": "Questões"})
+
+# Mapeando os valores de 'Questões' para números de 1 a 30
+questoes_mapping = {questao: i+1 for i, questao in enumerate(freq_questions_melted['Questões'].unique())}
+freq_questions_melted['Questões_Num'] = freq_questions_melted['Questões'].map(questoes_mapping)
+
+# Criando o gráfico de barras
+fig = px.bar(freq_questions_melted, x='Questões', y='Frequência', color='Respostas', title="Frequência das Respostas por Questão", width=1000, height=600)
+fig.update_xaxes(tickvals=list(range(0, 31)))
+st.plotly_chart(fig)
+
+
 # Create a sidebar
 st.sidebar.title("Filtros")
 
@@ -48,16 +81,15 @@ st.sidebar.write("###### *DCC = Técnico em Desenho de Construção Civil")
 st.sidebar.write("###### **EDI = Técnico em Edificações")
 
 df_sidebar1 = df_filtered[df_filtered["Curso"] == course]
+
+new_column_names = {old_name: f"Q{new_name}" for old_name, new_name in zip(df_sidebar1.columns[1:], range(1, len(df_sidebar1.columns)))}
+df_sidebar1 = df_sidebar1.rename(columns=new_column_names)
+
 st.write(df_sidebar1)
 
 subject = st.sidebar.selectbox("Selecione o assunto", df_subject["Assunto"].unique())
 df_sidebar2 = df_subject[df_subject["Assunto"] == subject]
 
-# Displaying general answers frequency to the questions
-st.write("## Frequência geral das respostas por pergunta")
-df_questions = df_sidebar1.iloc[:, 1:]
-freq_questions = df_questions.apply(pd.Series.value_counts).fillna(0)
-st.write(freq_questions)
 
 # Configuring the layout
 if subject == "Gênero e Sexualidade":
@@ -91,44 +123,44 @@ if subject == "Gênero e Sexualidade":
     freq_q9 = df_q9.value_counts()
     freq_q10 = df_q10.value_counts()
 
-    fig1 = px.pie(freq_q1, values=freq_q1.values, names=freq_q1.index, title="Questão 1")
-    fig1.update_layout(title = df_questions_list["Questões"][0])
+    fig1 = px.pie(freq_q1, values=freq_q1.values, names=freq_q1.index)
+    fig1.update_layout(title = dict(text=df_sidebar1.columns[1]), font=dict(size=12))
     col1.plotly_chart(fig1, use_container_width=True)
 
-    fig2 = px.pie(freq_q2, values=freq_q2.values, names=freq_q2.index, title="Questão 2")
-    fig2.update_layout(title = df_questions_list["Questões"][1])
+    fig2 = px.pie(freq_q2, values=freq_q2.values, names=freq_q2.index)
+    fig2.update_layout(title = dict(text=df_sidebar1.columns[2]), font=dict(size=12))
     col2.plotly_chart(fig2, use_container_width=True)
 
-    fig3 = px.pie(freq_q3, values=freq_q3.values, names=freq_q3.index, title="Questão 3")
-    fig3.update_layout(title = df_questions_list["Questões"][2])
+    fig3 = px.pie(freq_q3, values=freq_q3.values, names=freq_q3.index)
+    fig3.update_layout(title = dict(text=df_sidebar1.columns[3]), font=dict(size=12))
     col3.plotly_chart(fig3, use_container_width=True)
 
-    fig4 = px.pie(freq_q4, values=freq_q4.values, names=freq_q4.index, title="Questão 4")
-    fig4.update_layout(title = df_questions_list["Questões"][3])
+    fig4 = px.pie(freq_q4, values=freq_q4.values, names=freq_q4.index)
+    fig4.update_layout(title = dict(text=df_sidebar1.columns[4]), font=dict(size=12))
     col4.plotly_chart(fig4, use_container_width=True)
 
-    fig5 = px.pie(freq_q5, values=freq_q5.values, names=freq_q5.index, title="Questão 5")
-    fig5.update_layout(title = df_questions_list["Questões"][4])
+    fig5 = px.pie(freq_q5, values=freq_q5.values, names=freq_q5.index)
+    fig5.update_layout(title = dict(text=df_sidebar1.columns[5]), font=dict(size=12))
     col5.plotly_chart(fig5, use_container_width=True)
 
-    fig6 = px.pie(freq_q6, values=freq_q6.values, names=freq_q6.index, title="Questão 6")
-    fig6.update_layout(title = df_questions_list["Questões"][5])
+    fig6 = px.pie(freq_q6, values=freq_q6.values, names=freq_q6.index)
+    fig6.update_layout(title = dict(text=df_sidebar1.columns[6]), font=dict(size=12))
     col6.plotly_chart(fig6, use_container_width=True)
 
-    fig7 = px.pie(freq_q7, values=freq_q7.values, names=freq_q7.index, title="Questão 7")
-    fig7.update_layout(title = df_questions_list["Questões"][6])
+    fig7 = px.pie(freq_q7, values=freq_q7.values, names=freq_q7.index)
+    fig7.update_layout(title = dict(text=df_sidebar1.columns[7]), font=dict(size=12))
     col7.plotly_chart(fig7, use_container_width=True)
 
-    fig8 = px.pie(freq_q8, values=freq_q8.values, names=freq_q8.index, title="Questão 8")
-    fig8.update_layout(title = df_questions_list["Questões"][7])
+    fig8 = px.pie(freq_q8, values=freq_q8.values, names=freq_q8.index)
+    fig8.update_layout(title = dict(text=df_sidebar1.columns[8]), font=dict(size=12))
     col8.plotly_chart(fig8, use_container_width=True)
 
-    fig9 = px.pie(freq_q9, values=freq_q9.values, names=freq_q9.index, title="Questão 9")
-    fig9.update_layout(title = df_questions_list["Questões"][8])
+    fig9 = px.pie(freq_q9, values=freq_q9.values, names=freq_q9.index)
+    fig9.update_layout(title = dict(text=df_sidebar1.columns[9]), font=dict(size=12))
     col9.plotly_chart(fig9, use_container_width=True)
 
-    fig10 = px.pie(freq_q10, values=freq_q10.values, names=freq_q10.index, title="Questão 10")
-    fig10.update_layout(title = df_questions_list["Questões"][9])
+    fig10 = px.pie(freq_q10, values=freq_q10.values, names=freq_q10.index)
+    fig10.update_layout(title = dict(text=df_sidebar1.columns[10]), font=dict(size=12))
     col10.plotly_chart(fig10, use_container_width=True)
 
 
@@ -167,40 +199,40 @@ elif subject == "Racismo":
     fig11.update_layout(title = df_questions_list["Questões"][10])
     col11.plotly_chart(fig11, use_container_width=True)
 
-    fig12 = px.pie(freq_q12, values=freq_q12.values, names=freq_q12.index, title="Questão 12")
-    fig12.update_layout(title = df_questions_list["Questões"][11])
+    fig12 = px.pie(freq_q12, values=freq_q12.values, names=freq_q12.index)
+    fig12.update_layout(title = dict(text=df_sidebar1.columns[12]), font=dict(size=12))
     col12.plotly_chart(fig12, use_container_width=True)
 
-    fig13 = px.pie(freq_q13, values=freq_q13.values, names=freq_q13.index, title="Questão 13")
-    fig13.update_layout(title = df_questions_list["Questões"][12])
+    fig13 = px.pie(freq_q13, values=freq_q13.values, names=freq_q13.index)
+    fig13.update_layout(title = dict(text=df_sidebar1.columns[13]), font=dict(size=12))
     col13.plotly_chart(fig13, use_container_width=True)
 
-    fig14 = px.pie(freq_q14, values=freq_q14.values, names=freq_q14.index, title="Questão 14")
-    fig14.update_layout(title = df_questions_list["Questões"][13])
+    fig14 = px.pie(freq_q14, values=freq_q14.values, names=freq_q14.index)
+    fig14.update_layout(title = dict(text=df_sidebar1.columns[14]), font=dict(size=12))
     col14.plotly_chart(fig14, use_container_width=True)
 
-    fig15 = px.pie(freq_q15, values=freq_q15.values, names=freq_q15.index, title="Questão 15")
-    fig15.update_layout(title = df_questions_list["Questões"][14])
+    fig15 = px.pie(freq_q15, values=freq_q15.values, names=freq_q15.index)
+    fig15.update_layout(title = dict(text=df_sidebar1.columns[15]), font=dict(size=12))
     col15.plotly_chart(fig15, use_container_width=True)
 
-    fig16 = px.pie(freq_q16, values=freq_q16.values, names=freq_q16.index, title="Questão 16")
-    fig16.update_layout(title = df_questions_list["Questões"][15])
+    fig16 = px.pie(freq_q16, values=freq_q16.values, names=freq_q16.index)
+    fig16.update_layout(title = dict(text=df_sidebar1.columns[16]), font=dict(size=12))
     col16.plotly_chart(fig16, use_container_width=True)
 
-    fig17 = px.pie(freq_q17, values=freq_q17.values, names=freq_q17.index, title="Questão 17")
-    fig17.update_layout(title = df_questions_list["Questões"][16])
+    fig17 = px.pie(freq_q17, values=freq_q17.values, names=freq_q17.index)
+    fig17.update_layout(title = dict(text=df_sidebar1.columns[17]), font=dict(size=12))
     col17.plotly_chart(fig17, use_container_width=True)
 
-    fig18 = px.pie(freq_q18, values=freq_q18.values, names=freq_q18.index, title="Questão 18")
-    fig18.update_layout(title = df_questions_list["Questões"][17])
+    fig18 = px.pie(freq_q18, values=freq_q18.values, names=freq_q18.index)
+    fig18.update_layout(title = dict(text=df_sidebar1.columns[18]), font=dict(size=12))
     col18.plotly_chart(fig18, use_container_width=True)
 
-    fig19 = px.pie(freq_q19, values=freq_q19.values, names=freq_q19.index, title="Questão 19")
-    fig19.update_layout(title = df_questions_list["Questões"][18])
+    fig19 = px.pie(freq_q19, values=freq_q19.values, names=freq_q19.index)
+    fig19.update_layout(title = dict(text=df_sidebar1.columns[19]), font=dict(size=12))
     col19.plotly_chart(fig19, use_container_width=True)
 
-    fig20 = px.pie(freq_q20, values=freq_q20.values, names=freq_q20.index, title="Questão 20")
-    fig20.update_layout(title = df_questions_list["Questões"][19])
+    fig20 = px.pie(freq_q20, values=freq_q20.values, names=freq_q20.index)
+    fig20.update_layout(title = dict(text=df_sidebar1.columns[20]), font=dict(size=12))
     col20.plotly_chart(fig20, use_container_width=True)
 
 else:
@@ -234,43 +266,43 @@ else:
     freq_q29 = df_q29.value_counts()
     freq_q30 = df_q30.value_counts()
 
-    fig21 = px.pie(freq_q21, values=freq_q21.values, names=freq_q21.index, title="Questão 21")
-    fig21.update_layout(title = df_questions_list["Questões"][20])
+    fig21 = px.pie(freq_q21, values=freq_q21.values, names=freq_q21.index)
+    fig21.update_layout(title = dict(text=df_sidebar1.columns[21]), font=dict(size=12))
     col21.plotly_chart(fig21, use_container_width=True)
 
-    fig22 = px.pie(freq_q22, values=freq_q22.values, names=freq_q22.index, title="Questão 22")
-    fig22.update_layout(title = df_questions_list["Questões"][21])
+    fig22 = px.pie(freq_q22, values=freq_q22.values, names=freq_q22.index)
+    fig22.update_layout(title = dict(text=df_sidebar1.columns[22]), font=dict(size=12))
     col22.plotly_chart(fig22, use_container_width=True)
 
-    fig23 = px.pie(freq_q23, values=freq_q23.values, names=freq_q23.index, title="Questão 23")
-    fig23.update_layout(title = df_questions_list["Questões"][22])
+    fig23 = px.pie(freq_q23, values=freq_q23.values, names=freq_q23.index)
+    fig23.update_layout(title = dict(text=df_sidebar1.columns[23]), font=dict(size=12))
     col23.plotly_chart(fig23, use_container_width=True)
 
-    fig24 = px.pie(freq_q24, values=freq_q24.values, names=freq_q24.index, title="Questão 24")
-    fig24.update_layout(title = df_questions_list["Questões"][23])
+    fig24 = px.pie(freq_q24, values=freq_q24.values, names=freq_q24.index)
+    fig24.update_layout(title = dict(text=df_sidebar1.columns[24]), font=dict(size=12))
     col24.plotly_chart(fig24, use_container_width=True)
 
-    fig25 = px.pie(freq_q25, values=freq_q25.values, names=freq_q25.index, title="Questão 25")
-    fig25.update_layout(title = df_questions_list["Questões"][24])
+    fig25 = px.pie(freq_q25, values=freq_q25.values, names=freq_q25.index)
+    fig25.update_layout(title = dict(text=df_sidebar1.columns[25]), font=dict(size=12))
     col25.plotly_chart(fig25, use_container_width=True)
 
-    fig26 = px.pie(freq_q26, values=freq_q26.values, names=freq_q26.index, title="Questão 26")
-    fig26.update_layout(title = df_questions_list["Questões"][25])
+    fig26 = px.pie(freq_q26, values=freq_q26.values, names=freq_q26.index)
+    fig26.update_layout(title = dict(text=df_sidebar1.columns[26]), font=dict(size=12))
     col26.plotly_chart(fig26, use_container_width=True)
 
-    fig27 = px.pie(freq_q27, values=freq_q27.values, names=freq_q27.index, title="Questão 27")
-    fig27.update_layout(title = df_questions_list["Questões"][26])
+    fig27 = px.pie(freq_q27, values=freq_q27.values, names=freq_q27.index)
+    fig27.update_layout(title = dict(text=df_sidebar1.columns[27]), font=dict(size=12))
     col27.plotly_chart(fig27, use_container_width=True)
 
-    fig28 = px.pie(freq_q28, values=freq_q28.values, names=freq_q28.index, title="Questão 28")
-    fig28.update_layout(title = df_questions_list["Questões"][27])
+    fig28 = px.pie(freq_q28, values=freq_q28.values, names=freq_q28.index)
+    fig28.update_layout(title = dict(text=df_sidebar1.columns[28]), font=dict(size=12))
     col28.plotly_chart(fig28, use_container_width=True)
 
-    fig29 = px.pie(freq_q29, values=freq_q29.values, names=freq_q29.index, title="Questão 29")
-    fig29.update_layout(title = df_questions_list["Questões"][28])
+    fig29 = px.pie(freq_q29, values=freq_q29.values, names=freq_q29.index)
+    fig29.update_layout(title = dict(text=df_sidebar1.columns[29]), font=dict(size=12))
     col29.plotly_chart(fig29, use_container_width=True)
 
-    fig30 = px.pie(freq_q30, values=freq_q30.values, names=freq_q30.index, title="Questão 30")
-    fig30.update_layout(title = df_questions_list["Questões"][29])
+    fig30 = px.pie(freq_q30, values=freq_q30.values, names=freq_q30.index)
+    fig30.update_layout(title = dict(text=df_sidebar1.columns[30]), font=dict(size=12))
     col30.plotly_chart(fig30, use_container_width=True)
 
